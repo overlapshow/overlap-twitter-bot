5263f4740736a8a09014fc3c6797b5e74f344347
Charlotte Dann <ask@pouretrebelle.com>
2017-07-10 22:49:01 +0100

    :art: Use artist object to match tweet text

--- src/Artists.js
+++ src/Artists.js
@@ -1,32 +1,142 @@
-module.exports = {
-  AGROTI          : "Thalia's work mainly revolves around experimental sound creation exploring immersive sonic environments. More info: http://overlap.show/artist/thalia-agroti/",
-  ALEXANDRI       : "Eleni is a visual artist focussing on the concept of the archive and archival art. More info: http://overlap.show/artist/eleni-alexandri/",
-  BONDARCIUKAS    : "Arturas is working with computer generated imagery, computational arts, and traditional photography. More info: http://overlap.show/artist/arcturas-bondarciukas/",
-  CARTWRIGHT      : "Amy is a dancercreating computational choreographies that use technology within live performance. More info: http://overlap.show/artist/amy-cartwright/",
-  DANN            : "Charlotte's work centres around nullifying the contradiction between being a maker and a programmer. More info: http://overlap.show/artist/charlotte-dann/",
-  DEKKER          : "Laura's work explores the reciprocal roles of technologies in how we construct ourselves and our world. More info: http://overlap.show/artist/laura-dekker/",
-  EDWARDS         : "Diane focuses on the human microbiome, digital/organic ontologies and questions of more-than-human life. More info: http://overlap.show/artist/diane-edwards/",
-  FREEKE          : "Saskia's art work focuses on structure, geometry and playfulness by creating novel, tangible interfaces. More info: http://overlap.show/artist/saskia-freeke/",
-  GAO             : "Miduo aims to create artworks that generate public awareness about environmental issues. More info: http://overlap.show/artist/miduo-gao/",
-  GLOCK           : "Jakob uses code to build generative systems inspired by nature, science, sound and graphics programming. More info: http://overlap.show/artist/jakob-glock/",
-  GREEKALOGERAKIS : "Georgios examines religion, art, gender, life and death while blending the imaginative and the real. More info: http://overlap.show/artist/georgios-greekalogerakis/",
-  HAEBICH         : "Jayson uses technology to create installations that speculate on present and future forms of technology. More info: http://overlap.show/artist/jayson-haebich/",
-  HALL_SMITH      : "Jade is interested in assistive and rehabilitative technology for people with disabilities. More info: http://overlap.show/artist/jade-hall-smith/",
-  HONG            : "Freddie explores methods of involving human bodies, and other qualitative matters, in architecture. More info: http://overlap.show/artist/freddie-hong/",
-  JUSTKA          : "Ewa researches exploration of materiality, ontological systems and modes of quasi-direct perception. More info: http://overlap.show/artist/ewa-justka/",
-  KANGSADANSENANON: "Natthakit is interested in merging intelligent digital technologies into existing objects of daily life. More info: http://overlap.show/artist/natthakit-kansadansenanon/",
-  KHATTAK         : "Mehrbano is exploring how creative coding and physical computing can enhance her interactive art. More info: http://overlap.show/artist/mehrbano-khattak/",
-  LIU             : "Philip, a multi-disciplinary artist, explores mathematical aesthetics and virtuality through his work. More info: http://overlap.show/artist/philip-liu/" ,
-  MARTINEZ        : "Alix reflects on universal emotions and the relationship between humans with technology. More info: http://overlap.show/artist/alix-martinez-martinez/",
-  FRIENDRED       : "Friendred focuses on a dialogue between spatiotemporal substances, sex and theology. More info: http://overlap.show/artist/friendred/",
-  MELNYCZUK       : "Howard explores creation, archiving and implementation of data structures to create alternate realities. More info: http://overlap.show/artist/howard-malnyczuk/",
-  PARK            : "Soon is a graphic designer and performance artist who has worked for today’s largest fashion brands. More info: http://overlap.show/artist/soon-park/",
-  RAHAT           : "Nadia creates interactive art works that investigate the relationships between humans and machines. More info: http://overlap.show/artist/nadia-rahat/",
-  RAHIMI          : "Julianne's practice focuses on user interactions and storytelling through various visual forms. More info: http://overlap.show/artist/julianne-rahimi/",
-  RECOULES_QUANG  : "Sabrina is exploring the similarities between shadow theatre, embodiment and computational practice. More info: http://overlap.show/artist/sabrina-recoules-quang/",
-  RUBIM           : "Luis creates interactive art and physical objects using code to exploring play and mimicry. More info: http://overlap.show/artist/luis-rubim/",
-  SON             : "Yeoul focuses on exploring the relationships between data sets and how they can be seen differently. More info: http://overlap.show/artist/yeoul-son/",
-  THOMPSON        : "Andrew is researching the dichotomy of work and play, and how that manifests itself in instrument design. More info: http://overlap.show/artist/andrew-thompson/",
-  
-  UNDEFINED       : "Oops, I didn't quite catch you there! Try again or read about our artists here: http://overlap.show/"
-}
+module.exports = [
+  {
+    keywords: ['thalia', 'thalia agroti'],
+    slug: 'thalia-agroti',
+    snippet: 'Thalia\'s work mainly revolves around experimental sound creation exploring immersive sonic environments.',
+  },
+  {
+    keywords: ['eleni', 'eleni alexandri'],
+    slug: 'eleni-alexandri',
+    snippet: 'Eleni is a visual artist focussing on the concept of the archive and archival art.',
+  },
+  {
+    keywords: ['arturas', 'arturas bondarciukas'],
+    slug: 'arturas bondarciukas',
+    snippet: 'Arturas is working with computer generated imagery, computational arts, and traditional photography.',
+  },
+  {
+    keywords: ['amy', 'amy cartwright'],
+    slug: 'amy-cartwright',
+    snippet: 'Amy is a dancercreating computational choreographies that use technology within live performance.',
+  },
+  {
+    keywords: ['charlotte', 'charlotte dann'],
+    slug: 'charlotte-dann',
+    snippet: 'Charlotte\'s work centres around nullifying the contradiction between being a maker and a programmer.',
+  },
+  {
+    keywords: ['laura', 'laura dekker'],
+    slug: 'laura-dekker',
+    snippet: 'Laura\'s work explores the reciprocal roles of technologies in how we construct ourselves and our world.',
+  },
+  {
+    keywords: ['diane', 'diane edwards'],
+    slug: 'diane-edwards',
+    snippet: 'Diane focuses on the human microbiome, digital/organic ontologies and questions of more-than-human life.',
+  },
+  {
+    keywords: ['saskia', 'saskia freeke'],
+    slug: 'saskia-freeke',
+    snippet: 'Saskia\'s art work focuses on structure, geometry and playfulness by creating novel, tangible interfaces.',
+  },
+  {
+    keywords: ['miduo', 'miduo gao'],
+    slug: 'miduo-gao',
+    snippet: 'Miduo aims to create artworks that generate public awareness about environmental issues.',
+  },
+  {
+    keywords: ['jakob', 'jakob glock'],
+    slug: 'jakob-glock',
+    snippet: 'Jakob uses code to build generative systems inspired by nature, science, sound and graphics programming.',
+  },
+  {
+    keywords: ['georgios', 'georgios greekalogerakis'],
+    slug: 'georgios-greekalogerakis',
+    snippet: 'Georgios examines religion, art, gender, life and death while blending the imaginative and the real.',
+  },
+  {
+    keywords: ['jayson', 'jayson haebich'],
+    slug: 'jayson haebich',
+    snippet: 'Jayson uses technology to create installations that speculate on present and future forms of technology.',
+  },
+  {
+    keywords: ['jade hall smith', 'jade hall smith hall-smith'],
+    slug: 'jade-hall-smith',
+    snippet: 'Jade is interested in assistive and rehabilitative technology for people with disabilities.',
+  },
+  {
+    keywords: ['freddie', 'freddie hong'],
+    slug: 'freddie-hong',
+    snippet: 'Freddie explores methods of involving human bodies, and other qualitative matters, in architecture.',
+  },
+  {
+    keywords: ['ewa', 'ewa justka'],
+    slug: 'ewa-justka',
+    snippet: 'Ewa researches exploration of materiality, ontological systems and modes of quasi-direct perception.',
+  },
+  {
+    keywords: ['natthakit', 'natthakit kangsadansenanon'],
+    slug: 'natthakit-kansadansenanon',
+    snippet: 'Natthakit is interested in merging intelligent digital technologies into existing objects of daily life.',
+  },
+  {
+    keywords: ['mehrbano', 'mehrbano khattak'],
+    slug: 'mehrbano-khattak',
+    snippet: 'Mehrbano is exploring how creative coding and physical computing can enhance her interactive art.',
+  },
+  {
+    keywords: ['philip', 'philip liu'],
+    slug: 'philip-liu',
+    snippet: 'Philip, a multi-disciplinary artist, explores mathematical aesthetics and virtuality through his work.',
+  },
+  {
+    keywords: ['alix', 'alix martinez'],
+    slug: 'alix-martinez',
+    snippet: 'Alix reflects on universal emotions and the relationship between humans with technology.',
+  },
+  {
+    keywords: ['friendred'],
+    slug: 'friendred',
+    snippet: 'Friendred focuses on a dialogue between spatiotemporal substances, sex and theology.',
+  },
+  {
+    keywords: ['howard', 'howard melnyczuk'],
+    slug: 'howard-melnyczuk',
+    snippet: 'Howard explores creation, archiving and implementation of data structures to create alternate realities.',
+  },
+  {
+    keywords: ['soon', 'soon park'],
+    slug: 'soon-park',
+    snippet: 'Soon is a graphic designer and performance artist who has worked for today’s largest fashion brands.',
+  },
+  {
+    keywords: ['nadia', 'nadia rahat'],
+    slug: 'nadia-rahat',
+    snippet: 'Nadia creates interactive art works that investigate the relationships between humans and machines.',
+  },
+  {
+    keywords: ['julianne', 'julianne rahimi'],
+    slug: 'julianne-rahimi',
+    snippet: 'Julianne\'s practice focuses on user interactions and storytelling through various visual forms.',
+  },
+  {
+    keywords: ['sabrina', 'sabrina recoules quang'],
+    slug: 'sabrina-recoules-quang',
+    snippet: 'Sabrina is exploring the similarities between shadow theatre, embodiment and computational practice.',
+  },
+  {
+    keywords: ['lius', 'lius rubim'],
+    slug: 'lius-rubim',
+    snippet: 'Luis creates interactive art and physical objects using code to exploring play and mimicry.',
+  },
+  {
+    keywords: ['yeoul', 'yeoul son'],
+    slug: 'yeoul-son',
+    snippet: 'Yeoul focuses on exploring the relationships between data sets and how they can be seen differently.',
+  },
+  {
+    keywords: ['andrew', 'andrew thompson'],
+    slug: 'andrew-thompson',
+    snippet: 'Andrew is researching the dichotomy of work and play, and how that manifests itself in instrument design.',
+  },
+];
